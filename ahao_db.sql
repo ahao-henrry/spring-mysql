eb7352fb7e302290456f3c959227723357798f1d
ahao-henrry <2577168720@qq.com>
2017-11-01 19:43:13 +0800

    加传一个sql脚本

---
+++ ahao_db.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.28 : Database - ahao_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ahao_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ahao_db`;
+
+/*Table structure for table `like_table` */
+
+DROP TABLE IF EXISTS `like_table`;
+
+CREATE TABLE `like_table` (
+  `userId` varchar(32) NOT NULL,
+  `userName` varchar(32) DEFAULT NULL,
+  `sports` varchar(32) DEFAULT NULL,
+  `fruits` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `like_table` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
